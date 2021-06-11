6c2e9f943a459350711805616d0d7d0d015db519
takeshikurata <takeshikurata19761002@outlook.jp>
2021-06-11 19:34:28 +0900

    Add Department edit

---
+++ WebContent/WEB-INF/views/departments/edit.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+    <c:param name="content">
+        <c:choose>
+            <c:when test="${department != null}">
+                <h2>id : ${department.id} の部署情報 編集ページ</h2>
+                <form method="POST" action="<c:out value='/departments/update' />">
+                    <c:import url="_form.jsp" />
+                </form>
+                <script>
+                    function confirmDestroy() {
+                        if(confirm("本当に削除してよろしいですか？")) {
+                            document.forms[1].submit();
+                        }
+                    }
+                </script>
+            </c:when>
+            <c:otherwise>
+                <h2>お探しのデータは見つかりませんでした。</h2>
+            </c:otherwise>
+        </c:choose>
+
+        <p><a href="<c:url value='/departments/index' />">一覧に戻る</a></p>
+    </c:param>
+</c:import>
---
+++ src/controllers/departments/DepartmentsEditServlet.java
@@ -0,0 +1,48 @@
+package controllers.departments;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Department;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class DepartmentsEditServlet
+ */
+@WebServlet("/departments/edit")
+public class DepartmentsEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DepartmentsEditServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Department d = em.find(Department.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("department", d);
+        request.setAttribute("_token", request.getSession().getId());
+        request.getSession().setAttribute("department_id", d.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/departments/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
